7a1943c8870845a0c74a5c5be25699fffa0e852b
Aluno0323 <Aluno0323@univas.edu.br>
2016-05-10 22:18:23 -0300

    Lista de NOmes

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var names = ['Alison', 'Fabiano', 'Luiz', 'Wellington'];
+
+
+function addAllNames() {
+    for (var i = 0; i < names.length; i++) {
+        var li = createListItem(names[i]);
+        addNameToList(li);
+    }    
+}
+
+
+
+function addNameToList(li) {
+    var ul = document.getElementById('list-name');
+    ul.appendChild(li);
+}
+
+function createListItem(name) {
+    var li = document.createElement('li');
+    li.innerHTML = name;
+    return li;
+}
+
+
+addAllNames();
